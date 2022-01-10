4c78ae01abe0eaee3ccaa4a010c9420d6d0e40dd
PTK0225\erlle <erller123@gmail.com>
2022-01-10 23:55:51 +0900

    코드잇 연습 및 이것저것 업로드 연습

---
+++ area.py
@@ -0,0 +1,11 @@
+PI = 3.14
+
+
+
+def circle(radius):
+    return PI * radius * radius
+
+
+def square(length):
+    return length * length
+
---
+++ run.py
@@ -0,0 +1,9 @@
+import area
+
+def square(length):
+    return 4 * length
+
+print(dir())
+
+print(square(3))
+print(area.square(3))
